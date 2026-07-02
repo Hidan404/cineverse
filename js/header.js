cae9407e87c021c9f7389969dbef7ec8db84831c
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-07-02 10:15:42 -0300

    feat: Atualizar estilos do cabeçalho e corrigir variáveis de cor

--- js/header.js
+++ js/header.js
@@ -1,8 +1,8 @@
 const menuToggle = document.getElementById("menu-toggle")
 const mobileMenu = document.getElementById("mobile-menu")
 const menuIcone = document.querySelector("#menu-toggle svg path")
-const hamburgerPath = "M4 6h16M4 12h16M4 18h16";
-const closePath = "M6 18L18 6M6 6l12 12";
+const hamburgerPath = "M4 6h16M4 12h16M4 18h16"
+const closePath = "M6 18L18 6M6 6l12 12"
 
 
 function toggleMenu() {
