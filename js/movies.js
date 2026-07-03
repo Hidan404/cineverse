5941d89ba3b6619dffd1f20b8711fcbd74bdd38f
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-07-02 22:24:53 -0300

    feat: atualizar o estilo do card de filme para melhorar a legibilidade e a consistência

--- js/movies.js
+++ js/movies.js
@@ -79,13 +79,13 @@ const moviesGrid = document.getElementById("movies-grid")
 
 function createCard(movie){
     const cardd = document.createElement("article")
-    cardd.className = "w-full flex flex-col  bg-[#1E293B] rounded-lg p-4 shadow-md !hover:shadow-lg !hover:-translate-y-2 transition-shadow duration-300"
+    cardd.className = " w-full flex flex-col  bg-[#1E293B] rounded-lg p-4 shadow-md hover:shadow-lg hover:-translate-y-2 transition-shadow duration-300"
     cardd.innerHTML = `
         <img src="${movie.poster}" alt="${movie.title}" class="w-full h-64 object-cover rounded-lg mb-4 overflow-hidden">
-        <h3 class="text-xl font-semibold mb-2">${movie.title}</h3>
-        <p class="text-gray-400 mb-1">Ano: ${movie.year}</p>
-        <p class="text-gray-400 mb-1">Gênero: ${movie.genre}</p>
-        <p class="text-gray-400">Avaliação: ${movie.rating}</p>
+        <h3 class="!ml-2 text-xl font-bold text-[var(--color-texto)] !mb-2 !mt-4">${movie.title}</h3>
+        <p class="!ml-2 text-[var(--color-texto-secundario)] !mb-1">Ano: ${movie.year}</p>
+        <p class="!ml-2 text-[var(--color-texto-secundario)] !mb-1">Gênero: ${movie.genre}</p>
+        <p class="!ml-2 text-[var(--color-secundaria)] font-semibold !mb-2">Avaliação: ${movie.rating}</p>
     `
     return cardd
 }
