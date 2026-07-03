7e4f9567dd880092ae1d8e72e8988567719c51f6
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-07-02 22:06:49 -0300

    Refact: estrutura de código para melhor legibilidade e manutenibilidade

--- js/main.js
+++ js/main.js
@@ -1,2 +1,4 @@
 import { toggleMenu } from "./header.js"
+import { renderMovies } from "./movies.js"
 toggleMenu()
+renderMovies()
---
+++ js/movies.js
@@ -0,0 +1,103 @@
+const movies = [
+
+    {
+
+        title: "Interstellar",
+
+        year: 2014,
+
+        genre: "Sci-Fi",
+
+        rating: "8.7",
+
+        poster: "./assets/images/images.jpeg"
+
+    },
+    {
+
+        title: "Inception",
+
+        year: 2010,
+
+        genre: "Sci-Fi",
+
+        rating: "8.8",
+
+        poster: "./assets/images/images.jpeg"
+
+    },
+    {
+
+        title: "The Dark Knight",
+
+        year: 2008,
+
+        genre: "Action",
+
+        rating: "9.0",
+
+        poster: "./assets/images/images.jpeg"
+
+    },
+    {
+        title: "The Matrix",
+
+        year: 1999,
+
+        genre: "Sci-Fi",
+
+        rating: "8.7",
+
+        poster: "./assets/images/images.jpeg"
+    },
+     {
+        title: "The Matrix",
+
+        year: 1999,
+
+        genre: "Sci-Fi",
+
+        rating: "8.7",
+
+        poster: "./assets/images/images.jpeg"
+    },
+    {
+        title: "The Matrix",
+
+        year: 1999,
+
+        genre: "Sci-Fi",
+
+        rating: "8.7",
+
+        poster: "./assets/images/images.jpeg"
+    }
+
+]
+
+const moviesGrid = document.getElementById("movies-grid")
+
+function createCard(movie){
+    const cardd = document.createElement("article")
+    cardd.className = "w-full flex flex-col  bg-[#1E293B] rounded-lg p-4 shadow-md !hover:shadow-lg !hover:-translate-y-2 transition-shadow duration-300"
+    cardd.innerHTML = `
+        <img src="${movie.poster}" alt="${movie.title}" class="w-full h-64 object-cover rounded-lg mb-4 overflow-hidden">
+        <h3 class="text-xl font-semibold mb-2">${movie.title}</h3>
+        <p class="text-gray-400 mb-1">Ano: ${movie.year}</p>
+        <p class="text-gray-400 mb-1">Gênero: ${movie.genre}</p>
+        <p class="text-gray-400">Avaliação: ${movie.rating}</p>
+    `
+    return cardd
+}
+
+
+function renderMovies(){
+    moviesGrid.innerHTML = ""
+
+    movies.forEach(movie => {
+        const card = createCard(movie)
+        moviesGrid.appendChild(card)
+    })
+}
+
+export { renderMovies }
