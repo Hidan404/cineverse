c68fa745cc10cd0a6f0156eaefa062403dee2abc
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-07-07 16:36:53 -0300

    feat:adicionar section das categorias e implementar

---
+++ js/categoriesCards.js
@@ -0,0 +1,44 @@
+const categories = [
+    "Action",
+    "Comedy",
+    "Horror",
+    "Sci-Fi",
+    "Romance",
+    "Thriller"
+];
+
+const emojis = {
+    Action: "🎬",
+    Comedy: "😂",
+    Horror: "👻",
+    "Sci-Fi": "🚀",
+    Romance: "❤️",
+    Thriller: "🕵️"
+};
+
+
+
+function criarCardCategorias() {
+    const categorias = document.getElementById("categoriesCards")
+    categories.forEach(category => {
+        const card = document.createElement("div")
+        card.className = "w-full max-w-[200px]! cursor-pointer bg-slate-800 hover:bg-red-600 duration-300 rounded-xl p-8 text-center shadow-lg hover:scale-105"
+        card.innerHTML = `
+        <h3 class="mb-2! mt-2!">${emojis[category]}</h3>
+        <p class="mb-2!">${category}</p>
+    `
+        card.addEventListener("click", () => {
+
+            searchMovie(category);
+
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            })
+
+        })
+        categorias.appendChild(card)
+    })
+}
+
+export {criarCardCategorias}
--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ import { toggleMenu } from "./header.js"
 import { renderMovies } from "./movies.js"
 import { featuredMovies } from "./api.js"
 import { inicializarProcura } from "./search.js"
+import { criarCardCategorias } from "./categoriesCards.js"
 toggleMenu()
 renderMovies(featuredMovies)
 inicializarProcura()
+criarCardCategorias()
 
 
 
