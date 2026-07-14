8fd9ba000e868af4cdffbf4160b064e9ff6c24ef
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-07-14 16:37:17 -0300

    feat: Melhorar o layout do formulário de contato e adicionar vínculos dos campos de entrada para o endereço

---
+++ js/viacep.js
@@ -0,0 +1,4 @@
+const cepInput = document.getElementById("cep");
+const streetInput = document.getElementById("street");
+const cityInput = document.getElementById("city");
+const stateInput = document.getElementById("state");
