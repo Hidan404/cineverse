dfa7992b4c653a2417d5e3f135bab7a220e3a059
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-07-15 16:15:22 -0300

    feat: adicionar viacep função e msg de erro ao consultar cep

--- js/viacep.js
+++ js/viacep.js
@@ -1,4 +1,56 @@
-const cepInput = document.getElementById("cep");
-const streetInput = document.getElementById("street");
-const cityInput = document.getElementById("city");
-const stateInput = document.getElementById("state");
+const cepInput = document.getElementById("cep")
+const ruaInput = document.getElementById("street")
+const cidadeInput = document.getElementById("city")
+const estadoInput = document.getElementById("state")
+const msgErro = document.getElementById("msg-erro")
+const form = document.querySelector("form")
+
+
+async function buscarCep(cep) {
+
+    try{
+        const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        const dados = await resposta.json()
+
+        if(dados.erro){
+            throw new Error("Cep não encontrado")
+        }
+
+        ruaInput.value = dados.logradouro || ""
+        cidadeInput.value = dados.localidade || ""
+        estadoInput.value = dados.uf || ""
+    }catch (Error){
+        ruaInput.value = ""
+        cidadeInput.value = ""
+        estadoInput.value = ""
+
+        msgErro.className = "text-red-800 text-bold"
+        msgErro.innerHTML = "Cep não encontrado"
+    }
+    
+
+
+
+}
+
+
+function cepEvento(){
+    cepInput.addEventListener("input", (event) => {
+
+    const cep = event.target.value.replace(/\D/g, "");
+
+    if (cep.length === 8) {
+        buscarCep(cep);
+    }
+
+    });
+}
+
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+});
+
+export {cepEvento}
