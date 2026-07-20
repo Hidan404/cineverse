577416365816bac19f7322d86ba9223e31b3b376
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-07-20 17:04:47 -0300

    refactor: rename classesTailwind to classesTailwindcss for clarity

--- js/Estilotitulos.js
+++ js/Estilotitulos.js
@@ -1,10 +1,10 @@
 const titulosH2 = document.querySelectorAll('h2')
-const classesTailwind = ['text-4xl!', 'font-bold', 'text-[#F8FAFC]']
+const classesTailwindcss = ['text-4xl!', 'font-bold', 'text-[#F8FAFC]']
 
 
 function padronizarH2(){
     titulosH2.forEach(h2 => {
-        h2.classList.add(...classesTailwind)
+        h2.classList.add(...classesTailwindcss)
     })
 }
 
